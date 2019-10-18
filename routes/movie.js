2f2a5bd2886761cbee9b166629d4549aefe6feb9
Serhat Kerem EROL <keremerol.ce@outlook.com>
2019-10-18 18:54:49 +0300

    update

--- routes/movie.js
+++ routes/movie.js
@@ -1,53 +1,96 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 //models
 
-const Movie=require('../models/Movie');
-router.get('/',(req,res)=>{
-  const promise=Movie.find({});
-    promise.then((data)=>{
+const Movie = require("../models/Movie");
+router.get("/", (req, res) => {
+  const promise = Movie.find({});
+  promise
+    .then(data => {
       res.json(data);
-    }).catch((err)=>{
+    })
+    .catch(err => {
       res.json(err);
     });
 });
-router.get('/:movie_id',(req,res,next)=>{
-  const promise=Movie.findById(req.params.movie_id);
+router.get("/:movie_id", (req, res, next) => {
+  const promise = Movie.findById(req.params.movie_id);
 
-  promise.then((movie)=>{
-    if(!movie){
-      next('The movie was not found');
+  promise
+    .then(movie => {
+      if (!movie) {
+        next({ message: "The movie was not found", code: 99 });
+      }
+      res.json(movie);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+  //res.send(req.params);
+});
+router.put("/:movie_id", (req, res, next) => {
+  const promise = Movie.findByIdAndUpdate(
+    req.params.movie_id,
+    req.body,
+    {
+      new:true
     }
-    res.json(movie);
-  }).catch((err)=>{
-    res.json(err);
-  })
+    );
+
+  promise
+    .then(movie => {
+      if (!movie) {
+        next({ message: "The movie was not found", code: 99 });
+      }
+      res.json(movie);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+  //res.send(req.params);
+});
+router.delete("/:movie_id", (req, res, next) => {
+  const promise = Movie.findByIdAndRemove(req.params.movie_id);
+  promise
+    .then(movie => {
+      if (!movie) {
+        next({ message: "The movie was not found", code: 99 });
+      }
+      res.json(movie);
+    })
+    .catch(err => {
+      res.json(err);
+    });
   //res.send(req.params);
 });
 
 /* GET users listing. */
-router.post('/', function(req, res, next) {
- // const {title,imdb_score,category,country,year}=req.body;
-  const movie=new Movie(/*{
+router.post("/", function(req, res, next) {
+  // const {title,imdb_score,category,country,year}=req.body;
+  const movie = new Movie(
+    /*{
   /*  title:title,
     imdb_score:imdb_score,
     category:category,
     country:country,
     year:year
-  }*/ req.body);
- /* movie.save((err,data)=>{
+  }*/ req.body
+  );
+  /* movie.save((err,data)=>{
     if(err){
       res.json(err);
     }
     res.json(data);
   });*/
-  const promise=movie.save();
-  promise.then((data)=>{
-    res.json({status:1});
-  }).catch((err)=>{
-    res.json(err);
-  })
+  const promise = movie.save();
+  promise
+    .then(data => {
+      res.json({ status: 1 });
+    })
+    .catch(err => {
+      res.json(err);
+    });
 });
 
 module.exports = router;
