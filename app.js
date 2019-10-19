07dd334dfe0cefdbcf15a198f05877c02e8071d8
Serhat Kerem EROL <keremerol.ce@outlook.com>
2019-10-19 16:27:49 +0300

    verify-token update

--- app.js
+++ app.js
@@ -13,9 +13,13 @@ var app = express();
 
 //db connection
 const db=require('./helper/db.js')();
+
 //config connection
 const config=require('./config');
 app.set('api_secret_key',config.api_secret_key);
+
+//Middleware
+const verifyToken=require('./middleware/verify-token');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -30,6 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/api/movies', movie);
 app.use('/api/directors',director);
+app.use('/api',verifyToken);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
