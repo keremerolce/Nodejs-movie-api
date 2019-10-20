08ef1b4d4a699efe84be4b0b1a3b257e934dadcc
Serhat Kerem EROL <keremerol.ce@outlook.com>
2019-10-21 00:31:53 +0300

    Test update

--- test/api/movie.test.js
+++ test/api/movie.test.js
@@ -81,5 +81,57 @@ describe('api/movies tests',()=>{
         })
     })
 
+    describe('/PUT/:director_id movie', ()=>{
+        it('it should UPDATE a movie given by id',(done)=>{
+            const movie={
+                title:'93creative',
+                director_id:'5dab05085f77fd26ecad174d',
+                category:'SUÇ',
+                country:'fransa',
+                year:1970,
+                imdb_score:8
+            }
+            chai.request(server)
+                .put('/api/movies'+movieid)
+                .send(movie)
+                .set('x-access-token',token)
+                .end((err,res)=>{
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('title').eql(movie.title);
+                    res.body.should.have.property('director_id').eql(movie.director_id);
+                    res.body.should.have.property('category').eql(movie.category);
+                    res.body.should.have.property('country').eql(movie.country);
+                    res.body.should.have.property('year').eql(movie.year);
+                    res.body.should.have.property('imdb_score').eql(movie.imdb_score);
+                    
+                    done();
 
+                });
+        });
+    });
+
+    describe('/DELETE/:director_id movie', ()=>{
+        it('it should UPDATE a movie given by id',(done)=>{
+            const movie={
+                title:'creative',
+                director_id:'5dab05085f77fd26ecad174d',
+                category:'suç',
+                country:'fransa',
+                year:1970,
+                imdb_score:8
+            }
+            chai.request(server)
+                .delete('/api/movies'+movieid)
+                .set('x-access-token',token)
+                .end((err,res)=>{
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('status').eql(1);
+                    
+                    done();
+
+                });
+        });
+    });
 }); 
